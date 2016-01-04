4bea033ce4f4f9ddcf09286ec6017deeeda60b37
Jonatan Östling <jonatan.ostling@gmail.com>
2016-01-04 22:23:04 +0100

    Added filler questions

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@ var choiceA;
 var choiceB;
 var choiceC;
 var correct = 0;
+
+//Here is the questions for the quiz. I'm using multi dimentional arrays for this.
+//I'm storing the answer last - in the same array as the question and different choices
+var questions = [
+  [ "What is 1 + 1?", "0", "1", "2", "B" ],
+  [ "What is 1 + 1?", "0", "1", "2", "B" ],
+  [ "What is 1 + 1?", "0", "1", "2", "B" ],
+  [ "What is 1 + 1?", "0", "1", "2", "B" ],
+  [ "What is 1 + 1?", "0", "1", "2", "B" ],
+  [ "What is 1 + 1?", "0", "1", "2", "B" ],
+  [ "What is 1 + 1?", "0", "1", "2", "B" ],
+  [ "What is 1 + 1?", "0", "1", "2", "B" ],
+  [ "What is 1 + 1?", "0", "1", "2", "B" ]
+ //Filler questions to begin with. there are 9 in total.
+];
