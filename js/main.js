5731107620af037a38a9baff2067a311fe4a2071
Jonatan Östling <jonatan.ostling@gmail.com>
2016-01-04 22:18:20 +0100

    Added index, js file and css file
    Filled them with useful initial info.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Declaring variables I'm going to use later
+var position = 0;
+var test;
+var test_status;
+var question;
+var choice;
+var choices;
+var choiceA;
+var choiceB;
+var choiceC;
+var correct = 0;
